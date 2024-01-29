8e0a7136c2a8ee2ac008d3161e91d65a8b89374c
Jessica Sang <sangruoxuan@Sang-Ruoxuans-MacBook-Pro-2.local>
2024-01-29 15:02:48 -0800

    Complete compare.js in Basic JS

--- work/03-basic-js/compare.js
+++ work/03-basic-js/compare.js
@@ -6,5 +6,27 @@ function compare( word, guess ) {  // DO NOT MODIFY
 
 /* YOU MAY MODIFY THE LINES BELOW */
 
-  return 0; // this line is wrong
+word = word.toLowerCase();
+guess = guess.toLowerCase();
+
+const commonLetters = {};
+const wordLetterCount = {};
+
+for (let letter of word) {
+  wordLetterCount[letter] = (wordLetterCount[letter] || 0) + 1;
+}
+
+for (let letter of guess) {
+  if (wordLetterCount[letter] > 0) {
+    commonLetters[letter] = (commonLetters[letter] || 0) + 1;
+    wordLetterCount[letter] -= 1;
+  }
+}
+
+let totalCount = 0;
+for (let count of Object.values(commonLetters)) {
+  totalCount += count;
+}
+
+return totalCount;
 }
